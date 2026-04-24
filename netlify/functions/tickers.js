ccf8f3021aeb7b1c63dbb47a79f1373bd9f04532
improgram <isaac.macedo@yahoo.com.br>
2026-04-24 15:07:49 -0300

    function test return blobs

--- netlify/functions/tickers.js
+++ netlify/functions/tickers.js
@@ -18,21 +18,17 @@ export default async () => {
 
 
 /*
-Function anterior para limpeza dos blobs
-*/
+Function anterior para limpeza dos blobs:
+
 import * as netlifyBlobs from "@netlify/blobs";
-
 const getStore = netlifyBlobs?.getStore;
-
 export default async () => {
   if (typeof getStore !== "function") {
     return new Response("❌ getStore inválido");
   }
-
   const store = getStore({ name: "quotes-blobs" });
-
   await store.set("ticker-index", { value: 0 }, { type: "json" });
-
   const saved = await store.get("tickers-list", { type: "json" });
   return new Response(JSON.stringify(saved));
 };
+*/
