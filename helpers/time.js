cfcc022a2af01bfb2ff08120a6d80c192b2600a5
improgram <isaac.macedo@yahoo.com.br>
2026-05-29 19:28:01 -0300

    corrigir candle diario

--- helpers/time.js
+++ helpers/time.js
@@ -49,8 +49,8 @@ export const shouldRunNow = () => {
     // 10:15
     const afterStart = hour > 10 || (hour === 10 && minute >= 10);
 
-    // minutos antes de 20:00
-    const beforeEnd = hour < 20;
+    // minutos antes de 21:00
+    const beforeEnd = hour < 21;
 
     return isWeekDay && afterStart && beforeEnd;
 };
