ec6be4f06aa4210b5d342421a06cd6d4a32ee913
improgram <isaac.macedo@yahoo.com.br>
2026-06-22 15:41:01 -0300

    add USDB11

--- tickers-add.js
+++ tickers-add.js
@@ -59,7 +59,7 @@ const run = async () => {
     "CAML3", "CHIP11", "CMIN3", "DXCO3", "GRND3", "GOAT11",
     "HAPV3", "IMAB11", "IRFM11", "IVVB11", "JALL3", "KLBN4",
     "LFTB11", "NASD11", "NBIT11", "PACB11", "RAIL3", "RAIZ4",
-    "ROXO34", "SIMH3", "SLCE3", "SMAL11", "VIVT3", "5PRE11"
+    "ROXO34", "SIMH3", "SLCE3", "SMAL11", "USDB11", "VIVT3", "5PRE11"
   ]);
 };
 
